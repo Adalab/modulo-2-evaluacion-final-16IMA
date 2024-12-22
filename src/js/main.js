b1c54917401dde2c73544638dc541c5165b52909
16IMA <imonfort001@gmail.com>
2024-12-22 11:57:48 +0100

    queryselector create

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,19 @@
 
 /* Query selectors */
 const searchBox = document.querySelector(".js_searchBox")
+const searchBtn = document.querySelector(".js_searchBtn")
+const cards = document.querySelector(".js_cards")
+const cardsFavorites = document.querySelector(".js_cardsFavorites")
+const cardsCharacteres = document.querySelector(".js_cardsCharacteres")
+
+/* Event listeners */
+
+searchBtn.addEventListener("click", () => {}
+searchBox.addEventListener("click", () => {}
+
+
+
+/* Functions */
+
+/* API fetch */
+
