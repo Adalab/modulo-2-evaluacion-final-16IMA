5b0705d3549817a6b63698521557167ac46b125e
16IMA <imonfort001@gmail.com>
2024-12-23 12:39:55 +0100

    render function

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,52 @@ const cards = document.querySelector(".js_cards");
 const cardsFavorites = document.querySelector(".js_cardsFavorites");
 const cardsCharacteres = document.querySelector(".js_cardsCharacteres");
 
+/* Objetos*/
+
+let characters = [];
+let favorites = [];
+
+
 /* Event listeners */
 
 searchBtn.addEventListener("click", () => {});
 searchBox.addEventListener("click", () => {});
+cardsCharacteres.addEventListener("click", handleFavourite);
 
 
 
 /* Functions */
 
-/* API fetch */
+function renderOneCharacterCard(objCharacter) {
+    return `<li class="cards__box">
+            <img src="${objCharacter.imageUrl}" alt="character image ${objCharacter.name}"/>
+            <p>${objCharacter.name}</p>
+          </li>`;
+    
+};
 
+function renderAllCharactersCards() {
+    let html = "";
+    for(const objCharacter of characters){
+        html+= renderOneCharacterCard(objCharacter);
+    }
+    return cardsCharacteres.innerHTML=html;
+};
+
+const handleFavourite = (ev) => {
+  console.log('favorite');
+  console.log(ev.currentTarget);
+
+  
+  
+};
+
+
+/* API fetch */
+fetch('https://api.disneyapi.dev/character?pageSize=50').
+  then(response => response.json())
+  .then((data)=> {
+    characters=data.data;
+    renderAllCharactersCards();
+    console.table(characters);
+  })
