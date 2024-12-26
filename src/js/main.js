f6d336a4285f9b9b14cc710a8b735866a5449346
16IMA <imonfort001@gmail.com>
2024-12-26 13:01:37 +0100

    create fetch petitions fucntions and search eve btn / box

--- src/js/main.js
+++ src/js/main.js
@@ -11,25 +11,26 @@ cardsCharacteres.setAttribute("data-id"," ")
 
 let characters = [];
 let favorites = [];
-let charactersSearched = [];
 
+/* Eventos */
 
 searchBtn.addEventListener("click", () => {
     const searchTerm = searchBox.value.trim().toLowerCase();
-
-    fetch('https://api.disneyapi.dev/character?pageSize=50').
-    then(response => response.json())
-    .then((data)=> {
-        
-        charactersSearched = data.data.filter((char) => char.name.toLowerCase().includes(searchTerm));
-        
-        renderSearchedCards();
-        
-    });
-});
+  
+    if (searchTerm === "") {
+      fetchAllCharacters();
+    } else {
+      fetchSearchedCharacters(searchTerm);
+    }
+  });
 
 
 
+  searchBox.addEventListener("input", () => {
+    if (searchBox.value.trim() === "") {
+      fetchAllCharacters();
+    }
+  });
 
 
 /* Functions */
@@ -46,46 +47,45 @@ function renderOneCharacterCard(objCharacter) {
 
 /*Función renderiza todas las tarjetas*/
 
-function renderAllCharactersCards() {
+function renderAllCharactersCards(arrayOfCharacter) {
     let html = "";
-    for(const objCharacter of characters){
+    for(const objCharacter of arrayOfCharacter){
         html+= renderOneCharacterCard(objCharacter);
     }
     cardsCharacteres.innerHTML=html;
+    attachClickEventsToCards();
+};
 
 /*Añade evento click a cada <li>*/
 
+function attachClickEventsToCards() {
     const cardBox = document.querySelectorAll(".js_cardBox");
 
     for(const li of cardBox) {
         li.addEventListener("click", (ev) =>{
             ev.preventDefault();
             handleFavourite(ev);
-            
-            console.log(ev.currentTarget);
 
             /*Coge el atributo nuevo data-id como ancla para coger el objeto del array characters e incluirlo en el array favorites */
 
             const idCharacter = ev.currentTarget.getAttribute("data-id");
-            console.log(parseInt(idCharacter));
             const character = characters.find((char) => char._id === parseInt(idCharacter));
 
             /*Establece una condición if para evitar duplicados en el array favorites */
 
             if (!favorites.some((char) => char._id === character._id)) {
                 favorites.push(character);
-                console.log(favorites);
-
                 renderFavoritesCards();
 
                 /*Incluye en el localStorage el array favorites*/
 
                 localStorage.setItem('favorites', JSON.stringify(favorites));
-            }
+            };
             
         });
-    }
+    };
 };
+
 
 /*Función renderiza todas las tarjetas de favoritos */
 
@@ -95,14 +95,6 @@ function renderFavoritesCards() {
         html+= renderOneCharacterCard(objCharacter);
     }
     cardsFavorites.innerHTML=html;
-};
-
-function renderSearchedCards() {
-    let html = "";
-    for(const objCharacter of charactersSearched){
-        html+= renderOneCharacterCard(objCharacter);
-    }
-    cardsCharacteres.innerHTML=html;
 };
 
 /*Función añade fondo a la tarjeta favorita */
@@ -117,15 +109,34 @@ const handleFavourite = (ev) => {
 };
 
 
-/* API fetch */
-fetch('https://api.disneyapi.dev/character?pageSize=50').
-  then(response => response.json())
-  .then((data)=> {
-    characters=data.data;
-    renderAllCharactersCards();
-    console.table(characters);
+function fetchAllCharacters(){
+    fetch('https://api.disneyapi.dev/character?pageSize=50').
+    then(response => response.json())
+    .then((data)=> {
+        characters=data.data;
+        renderAllCharactersCards(characters);
+    });
+};
+        
+    
+function getFavoritesLocalstorage(){
     const savedFavorites = JSON.parse(localStorage.getItem('favorites'));
     favorites = favorites.concat(savedFavorites);
     renderFavoritesCards();
-        
-  })
+};
+
+function fetchSearchedCharacters(searchTerm) {
+    fetch("https://api.disneyapi.dev/character?pageSize=50")
+      .then((response) => response.json())
+      .then((data) => {
+          const filteredCharacters = data.data.filter((char) =>
+          char.name.toLowerCase().includes(searchTerm));
+        renderAllCharactersCards(filteredCharacters);
+    })
+      .catch((error) =>
+        console.error("Error al buscar personajes con término:", error)
+      );
+  }
+
+fetchAllCharacters();
+getFavoritesLocalstorage();
